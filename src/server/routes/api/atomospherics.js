3d2390068d533dcfa6068c71c41fe0a45f82048a
kaminchu <kmchu.mail@gmail.com>
2018-07-04 19:16:59 +0900

    順序を明示的に

--- src/server/routes/api/atomospherics.js
+++ src/server/routes/api/atomospherics.js
@@ -11,12 +11,15 @@ router.get("/", async (req, res) => {
   const {start, end} = req.query;
   const json = await (() => {
     const attributes = ["temperature", "humidity", "pressure", "time"];
+    const order = [["time", "ASC"]];
     if(!start && !end) {
       return Atmospheric.findAll({
+        order,
         attributes,
       });
     } else if (start && end) {
       return Atmospheric.findAll({
+        order,
         attributes,
         where: {
           time: {
@@ -27,6 +30,7 @@ router.get("/", async (req, res) => {
       });
     } else if (start) {
       return Atmospheric.findAll({
+        order,
         attributes,
         where: {
           time: {
@@ -36,6 +40,7 @@ router.get("/", async (req, res) => {
       });
     } else {
       return Atmospheric.findAll({
+        order,
         attributes,
         where: {
           time: {
